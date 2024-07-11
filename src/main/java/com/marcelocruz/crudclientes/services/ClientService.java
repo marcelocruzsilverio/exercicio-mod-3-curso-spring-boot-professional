e18feaf94a735af9bac1c0f5d8571c9264ea4c09
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-07-11 14:17:50 -0300

    Busca paginada de clientes

--- src/main/java/com/marcelocruz/crudclientes/services/ClientService.java
+++ src/main/java/com/marcelocruz/crudclientes/services/ClientService.java
@@ -4,6 +4,8 @@ import com.marcelocruz.crudclientes.dto.ClientDTO;
 import com.marcelocruz.crudclientes.entities.Client;
 import com.marcelocruz.crudclientes.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+
     }
 }
