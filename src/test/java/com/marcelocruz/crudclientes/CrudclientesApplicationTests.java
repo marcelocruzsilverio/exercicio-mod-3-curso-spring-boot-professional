f171dd44bc0fc0f1077e7dbf8e1f2f5c8e7c38cc
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-07-11 11:01:39 -0300

    Primeiro commit

---
+++ src/test/java/com/marcelocruz/crudclientes/CrudclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelocruz.crudclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
